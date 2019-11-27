23b51bc7f04b0a8a885ade5010a930f2f5be3ced
zwang123 <zhiwang@uchicago.edu>
2019-11-27 12:20:33 -0600

    crc

---
+++ flac/crc.h
@@ -0,0 +1,77 @@
+#ifndef __FLAC__CRC_H_INCLUDED
+#define __FLAC__CRC_H_INCLUDED
+#include <cstddef>
+#include <cstdint>
+
+//#include <iostream>
+
+namespace flac {
+  template <typename InputType, typename DivisorType,
+            std::size_t inputsize = sizeof(InputType) << 3,
+            std::size_t outputsize = sizeof(DivisorType) << 3
+           >
+  DivisorType crc_encode(InputType input, 
+                         DivisorType &output, 
+                         const DivisorType divisor)
+  {
+    // only lower bits are valid
+    static_assert(inputsize <= sizeof(input) << 3, "wrong inputsize");
+    static_assert(outputsize <= sizeof(output) << 3, "wrong outputsize");
+
+    if (inputsize < outputsize) {
+      InputType mask = output >> (outputsize - inputsize);
+      input ^= mask;
+      output <<= inputsize;
+    } else {
+      // more common
+      InputType mask = static_cast<InputType>(output)
+        << (inputsize - outputsize);
+      input ^= mask;
+      output = 0;
+    }
+
+    //std::cout << std::hex;
+    //std::cout << __FILE__ << __LINE__ << std::endl;
+    //std::cout << "in: " << (unsigned)input << std::endl;
+    //std::cout << "out: " << (unsigned)output << std::endl;
+
+    unsigned top_bit_pos = inputsize - 1;
+    InputType top_bit_mask = 1 << top_bit_pos;
+    while (input) {
+      if (input & top_bit_mask) {
+        input ^= top_bit_mask;
+        if (top_bit_pos < outputsize) {
+          // more common
+          input ^= static_cast<InputType>(divisor
+            >> (outputsize - top_bit_pos));
+          output ^= divisor << top_bit_pos;
+        } else {
+          input ^= static_cast<InputType>(divisor)
+            << (top_bit_pos - outputsize);
+        }
+      }
+      --top_bit_pos;
+      top_bit_mask >>= 1;
+    }
+
+    return output;
+  }
+
+  // CRC-8-CCITT
+  // divisor = 0x107
+  inline
+  uint8_t crc8_encode(uint8_t input, uint8_t &output, 
+      const uint8_t divisor = 7)
+  {
+    return crc_encode(input, output, divisor);
+  }
+  // CRC-16-IBM
+  // divisor = 0x18005
+  inline
+  uint16_t crc16_encode(uint8_t input, uint16_t &output, 
+      const uint16_t divisor = 0x8005)
+  {
+    return crc_encode(input, output, divisor);
+  }
+}
+#endif // __FLAC__CRC_H_INCLUDED
---
+++ flac/test_crc/main.cpp
@@ -0,0 +1,36 @@
+#include "../crc.h"
+#include <cassert>
+#include <cstdint>
+#include <iostream>
+
+int main()
+{
+  uint8_t codes[] ={0xff, 0xf8, 0xc9, 0x18, 0}; 
+  uint8_t codes2[12] ={0xff, 0xf8, 0xc9, 0x18, 0, 0xc2};
+  uint16_t codes3[] ={0xfff8, 0xc918, 0xc2};
+  uint8_t crc8 = 0;
+  uint16_t crc16 = 0;
+  uint8_t crc8_2 = 0;
+
+  for (const auto c : codes) {
+    flac::crc8_encode(c, crc8);
+  }
+  for (const auto c : codes2) {
+    flac::crc16_encode(c, crc16);
+  }
+  for (const auto c : codes3) {
+    flac::crc_encode(c, crc8_2, (uint8_t)0x7);
+  }
+
+  std::cout << std::hex;
+  std::cout << "crc8: " << (unsigned)crc8 << std::endl;
+  std::cout << "crc16: " << (unsigned)crc16 << std::endl;
+  std::cout << "crc8 2: " << (unsigned)crc8_2 << std::endl;
+
+  assert(crc8 == 0xc2);
+  assert(crc16 == 0xb8ee);
+  assert(crc8_2 == 0);
+
+  return 0;
+}
+
