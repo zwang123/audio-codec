bfabf1dd7d6bc37fd36b09d150ef418d41a092e1
zwang123 <zhiwang@uchicago.edu>
2019-11-28 19:49:59 -0600

    completely simplify istream_iterator

--- flac/istream_iterator.h
+++ flac/istream_iterator.h
@@ -1,45 +1,36 @@
 #include <istream>
-//#include <stdexcept>
 
 namespace flac {
+
+// the only usage: *iterator++
+
 class istream_iterator {
 public:
   // this is not an input iterator !
   using value_type = char;
-  using pointer = const value_type*;
   using reference = const value_type&;
   using istream_type = std::istream;
 
   // user should make sure there is only 1 istream_iterator with unread flag
   // associated with  each istream
   istream_iterator(istream_type &stream)
-    : is(stream), buffer(0), read_flag(false) {}
+    : is(stream), buffer(0) {}
   istream_iterator(const istream_iterator &) = default;
   ~istream_iterator() = default;
 
-  // user should make sure read_flag is true
   reference operator*() const {
     return buffer;
-  }
-  pointer operator->() const { return &operator*(); }
-
-  // do not copy after ++*this;
-  istream_iterator &operator++() {
-    if (read_flag) read_flag = false;
-    else is.get(buffer);
-    return *this;
   }
 
   // expect to be dereferenced soon
   istream_iterator operator++(int) {
-    auto rtn = operator++();
-    rtn.read_flag = true;
-    return rtn;
+    is.get(buffer);
+    return *this;
   }
 
 private:
   istream_type &is;
-  value_type buffer;
+  value_type buffer; // store the prev value
   bool read_flag;
 };
 }
